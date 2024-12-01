2334cdf2f9ce139a4f358cd554c419630ac0e41c
ranafge <ranafge@gmail.com>
2024-12-01 21:16:40 +0600

    for ci/cd github action

---
+++ tests/Acceptance/FirstCest.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace Tests\Acceptance;
+
+use Tests\Support\AcceptanceTester;
+
+class FirstCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+        $I->amOnPage("/wp-login.php");
+        $I->fillField("#user_login", "rana");
+        $I->fillField("#user_pass","Pass1234@");
+        $I->click("#wp-submit");
+
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+        $I->wantToTest("I want to input an HTML '<h1>Samsul</h1>' tag into the 'Full Name' field on the user form and check for a warning 'Full Name consist of only letters and dots.' message for this field.");
+        $I->amOnPage("/wp-admin/options-general.php?page=qa-test-settings");
+        $I->wait(.5);
+        $I->fillField("input#qa_test_fullname", "<h1>Samsul</h1>");
+        $I->fillField("input#qa_test_nickname", "Rana");
+        $I->fillField("input#qa_test_address", "paik");
+        $I->fillField("input#qa_test_dob_d", '32');
+        $I->selectOption('select#qa_test_dob_m', "7");  // December
+        $I->fillField('input#qa_test_dob_y', "2008");
+        $I->fillField("input#qa_test_email", 'wasir@admin.com');
+        $I->fillField("input#qa_test_web", 'https://wasir.com');
+        $I->click('//input[@type="submit"]');
+        $I->see("Full Name consist of only letters and dots.");
+        $I->wait(1);
+    }
+}
---
+++ tests/Functional/DBcrudCest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Functional;
+
+use Tests\Support\FunctionalTester;
+use \Codeception\Step\Argument\PasswordArgument;
+
+class UserFormSubmissionCest
+{
+    public function _before(FunctionalTester $I)
+    {
+        $I->amOnPage("/wp-login.php");
+        $I->fillField('//input[@type="text"]', "rana");
+        $I->fillField('//input[@type="password"]', new PasswordArgument("Pass1234@"));
+        $I->checkOption('#rememberme');
+        $I->click('//input[@type="submit"]');
+       
+    }
+
+    // tests
+    public function tryToTestSubmitUserFormDataInDatabase(FunctionalTester $I)
+    {
+        $I->wantToTest("I want to check if the saved user form data is correctly stored in the database.");
+        $I->amOnPage("/wp-admin/options-general.php?page=qa-test-settings");
+        $I->fillField("input#qa_test_fullname", "TestForFullname");
+        $I->fillField("input#qa_test_nickname", "TestForNicename");
+        $I->fillField("input#qa_test_address", "TestForAddress");
+        $I->fillField("input#qa_test_dob_d", '4');
+        $I->selectOption('select#qa_test_dob_m', "7");  // December
+        $I->fillField('input#qa_test_dob_y', "2008");
+        $I->fillField("input#qa_test_email", 'Test@admin.com');
+        $I->fillField("input#qa_test_web", 'https://test.com');
+        $I->click('//input[@type="submit"]');
+        $I->see("Dashboard");
+    }
+    public function tryToTestUserFormDataFullnameFromDatabase(FunctionalTester $I)
+    {
+
+        $I->wantToTest('I want to check if the saved full name data exists in the database.');
+        $optionName = 'qa_test_options';
+        $optionData = $I->grabFromDatabase('wp_options', 'option_value', ['option_name' => $optionName]);
+        $data = unserialize($optionData);
+        $I->assertEquals("TestForFullname", $data["qa_test_fullname"], "Full name exists in database");
+        codecept_debug($data);
+    }
+
+
+  }
